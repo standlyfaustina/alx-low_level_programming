f4d0ba642f41f9a052752d4a56544e91baad6733
standly faustina <standlyfaustina@gmail.com>
2022-10-19 01:47:35 -0700

    advd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int size;
+	int standard_error = 2;
+
+	size =
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(standard_error,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	size);
+	return (1);
+}
+
